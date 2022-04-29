7507f2bb4185e08cca31943492dee90630a57805
Himanshu Saini <himanshu@pop-os.localdomain>
2022-04-29 21:44:48 +0530

    session 21

---
+++ Session 21/index.js
@@ -0,0 +1,13 @@
+// hello this is a javascript file
+
+//inner html method
+//document.getElementById('jehu').innerHTML = 'hello we have written this using javascript';
+document.getElementById('jehu').innerHTML = 5+18;
+
+//document write method
+
+document.write('hello i am jehu')
+
+//alert method
+
+window.alert(77+69)
