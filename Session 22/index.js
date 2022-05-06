26e5818b8d6872c9925797117e9565606bd2eb1f
Himanshu Saini <himanshu@pop-os.localdomain>
2022-05-06 22:00:52 +0530

    session 22

---
+++ Session 22/index.js
@@ -0,0 +1,43 @@
+document.write('Hello fellas' +'<br>')
+
+
+let x = 'Jehu' // we have written jehu in quotes, jehu it is string datatype
+let y = 12 // numnber datatype are not written in quotes
+
+document.write('Hi my name is '+ x + ' and my age is ' + y+ ' years old')
+document.write('<br>')
+
+const radius = 20
+
+document.write('Circle diameter is twice the ' + radius + ' and it is actually ' + 2*radius) 
+
+var u = 40
+var h = null
+document.write('<br>')
+document.write(h)
+
+var c = 90
+var d = 90
+
+document.write('<br>')
+document.write(c!=d)  // compare two values
+
+var k = 9
+
+document.write('<br>')
+document.write(k-= 7) // k+= 7     k = k+ 7   k-=   k= k-7
+
+
+var f = 'kool' // str1
+var m = 'monkey' //str 3
+
+document.write('<br>')
+document.write(f+ ' ' +m)
+
+
+var p = 8
+
+document.write('<br>')
+document.write(--p) // p = p+1
+
+
