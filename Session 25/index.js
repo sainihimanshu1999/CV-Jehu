3d35269a504705e0b565727589941d5fc0beaeba
Himanshu Saini <himanshu@pop-os.localdomain>
2022-06-17 21:38:16 +0530

    session 25

---
+++ Session 25/index.js
@@ -0,0 +1,41 @@
+document.write('Hello jehu, welcome back from the trip','<br>')
+
+
+// func functionname(parameters/arguments){
+//      statement;
+//}
+
+function addition(a,b){
+    document.write(a+b,'<br>')
+}
+
+
+addition(4,2)
+addition(71,89)
+
+
+function jehu(a,b,c){
+    document.write(a+b+c,'<br>')
+    document.write('congrats, this function works')
+    return(a+b+c)
+}
+
+jehu(14,15,16)
+
+function hello(){
+    document.write('HEYOOOOOOO')
+    alert('TUTANKHAMUN WAS HERE')
+}
+
+hello()
+
+
+function info(a,b,c){
+    document.write('Hello, '+a+' my name is' +b+' i am '+c+' years old' )
+
+}
+
+info('Jehu','Himanshu','22')
+
+
+
